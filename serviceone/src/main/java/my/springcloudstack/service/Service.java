b2cb4aa54df5ffe08969cbe717bf6ef60acd1cd6
Amardeep Khera <amardeep.khera@sandstone.com.au>
2018-04-19 13:35:52 +1000

    Got it working

--- serviceone/src/main/java/my/springcloudstack/service/Service.java
+++ serviceone/src/main/java/my/springcloudstack/service/Service.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Service {
 
     @Autowired
+    private
     ServiceTwoClient serviceTwoClient;
 
     @RequestMapping("/hello")
